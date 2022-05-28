4cfa0740e0c0385e2ccec090b0be1ebf42fe94b9
student13 <student13@pcoip.ki.agh.edu.pl>
2022-05-28 10:49:18 +0200

    Lab ciag dalszy

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div id = "app">
+
+    <h1>Witaj w systemie do zapisów na zajęcia</h1>
     <h1>Twój e-mail:  {{email}}</h1>
 
+    <!--
     <div v-if="email.length < 10">Ale masz krótki adres!</div>
     <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
     <div v-else class="red">Twój adres e-mail jest stanowczo za długi.</div>
+    <input @keyup.enter = alertMyEmail() type = "email" v-model="email">
+    -->
 
-    <input type="text" v-model="email">
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+    <div v-if="logged == false">
+      <h3>Zaloguj się e-mailem {{email}}</h3>
+      <input type="email" v-model="email" >
+      <button @click="login()"> Wchodzę</button>
+    </div>
 
+    <div v-if="logged == true">
+      <h3>Witaj {{email}}</h3>
+      <button @click="logout()">Wyloguj</button>
+    </div>
+
+    <!-- <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button> -->
 
 
   </div>
@@ -20,7 +34,7 @@ export default {
   data() {
     return {
       email: '',
-      password: ''
+      logged: false
     };
   },
 
@@ -28,9 +42,18 @@ export default {
     alertMyEmail() {
       alert(this.email);
     }
+    ,
+
+    logout() {
+      this.logged = false;
+      this.email = "";
+    },
+
+    login() {
+      this.logged = true;
+    }
   }
 }
-
 
 </script>
 
