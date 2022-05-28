d42caedbd24b4b106968b5905fc9af63c46bb1d3
student13 <student13@pcoip.ki.agh.edu.pl>
2022-05-28 10:14:27 +0200

    Lab ciag dalszy

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id = "app">
-    <h1>e-mail:  {{email}}</h1>
+    <h1>Twój e-mail:  {{email}}</h1>
+
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else class="red">Twój adres e-mail jest stanowczo za długi.</div>
+
     <input type="text" v-model="email">
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+
+
 
   </div>
 </template>
 
 <script>
-
 
 export default {
   data() {
@@ -15,11 +22,20 @@ export default {
       email: '',
       password: ''
     };
-  }
+  },
 
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    }
+  }
 }
+
+
 </script>
 
 <style>
+
+.red { color: red; }
 
 </style>
